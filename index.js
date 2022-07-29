f6ec81cabb320738e7a4c0318d2690365528cb87
Tayyaba Kouser <tayyaba>
2022-07-29 16:49:18 +0530

    delete api

--- index.js
+++ index.js
@@ -54,6 +54,17 @@ app.put('/api/courses/:id', (req,res)=>{
 
 });
 
+app.delete('/api/courses/:id',(req,res)=>{
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The requested ID is not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+});
+
 function validationCourse(course){
     const schema = {
         name : Joi.string().min(3).required()
